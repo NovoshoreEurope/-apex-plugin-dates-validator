dcfc5e0b1cbac4994c47a079a152bd4839f37465
Juan Antonio Castellanos López <juan.antonio@novoshore.com>
2025-01-14 16:47:14 +0100

    update with changes in how we determine the diferences with the dates, use of apex.utils and apex.date libraries, add new format, messages and comments

--- src/jQuery/ns.plugin.js
+++ src/jQuery/ns.plugin.js
@@ -1,7 +1,4 @@
-//
-let usedEndDateInputs = [];
-
-// Error messages
+// Global variables to store error messages
 let MESSAGES = {
     NS_MUST_SELECT_DATE_FROM: '',
     NS_MUST_SELECT_DATE_TO: '',
@@ -9,17 +6,22 @@ let MESSAGES = {
     NS_EARLIER_THAN_DATE: ''
 };
 
-// Object containing date validation logic
+// Array to manage date input fields that have been used
+let usedEndDateInputs = [];
+
+// Object that contains the date validation logic
 var datesValidator = {
-    // Initializes the object, setting change event handlers on date inputs
+    // Initializes the validator, sets event handlers for the date inputs
     initialize: function (startDateInputId, endDateInputId, attr_must_select_date_from, attr_must_select_date_to, attr_greather_than_date, attr_earlier_than_date) {
         "use strict";
 
+        // Assign error messages from received attributes
         MESSAGES.NS_MUST_SELECT_DATE_FROM = attr_must_select_date_from;
         MESSAGES.NS_MUST_SELECT_DATE_TO = attr_must_select_date_to;
         MESSAGES.NS_GREATHER_THAN_DATE = attr_greather_than_date;
         MESSAGES.NS_EARLIER_THAN_DATE = attr_earlier_than_date;
 
+        // Add 'change' event handlers to the date input fields
         $(`#${startDateInputId}`).on("change", function (e) {
             validateDates(this, e, startDateInputId, endDateInputId);
         });
@@ -30,52 +32,77 @@ var datesValidator = {
     }
 };
 
-// Function to validate dates
+// Function to validate the dates
 function validateDates(item, event, startDateInput, endDateInput) {
-    // Function to obtain the date from a given input element
+    // Function to get the date from a given input element
     const getDate = (elementId) => {
-        const dateValue = $(`#${elementId}`).val();
-        return dateValue ? new Date(dateValue) : null;
+        const dateValue = $v(elementId);
+        const dateFormat = $x(elementId).getAttribute("format");
+        return dateValue ? apex.date.parse(dateValue, dateFormat) : null;
     };
 
-    // Function to clear error messages
+    // Función para limpiar los mensajes de error
     const clearError = (dateInput) => {
         $(`#${dateInput}_error_placeholder`).addClass('u-visible').html('');
     };
 
-    // Function to add error messages
+    // Function to clear error messages
     const addError = (dateInput, errorMessage) => {
         const errorTemplate = `<span class="t-Form-error"><div id="${dateInput}_error">${errorMessage}</div></span>`;
         $(`#${dateInput}_error_placeholder`).removeClass('u-visible').html(errorTemplate);
     };
 
-    // Get dates from input elements
+    // Get the dates from the input fields
     const startDate = getDate(startDateInput);
     const endDate = getDate(endDateInput);
 
-    // Validate if dates are valid
+    // If any of the dates is invalid, handle the errors
     if (!startDate || !endDate) {
+        // Clear previous errors
         clearError(startDateInput);
         clearError(endDateInput);
 
-        // Check if dates are used but not selected
+        // If both dates are not selected and both inputs have been used, clear the errors
         if (!startDate && !endDate && usedEndDateInputs.includes(endDateInput) && usedEndDateInputs.includes(startDateInput)) {
             clearError(startDateInput);
             clearError(endDateInput);
         } else {
-            // Handle errors when a date is missing
+            // Validate the start date
             if (!startDate) {
                 addError(startDateInput, MESSAGES.NS_MUST_SELECT_DATE_FROM);
             } else {
+                // If the start date is valid, mark it as used
                 usedEndDateInputs.push(startDateInput);
             }
 
+            // Validate the end date
             if (!endDate) {
-                // Copy the start date if available and not used previously
+                // If the end date is not selected and the start date is available, copy the start date to the end date
                 if (startDate && !usedEndDateInputs.includes(endDateInput)) {
-                    $(`#${endDateInput}`).val($(`#${startDateInput}`).val());
+                    // Get the value and format of the start date input
+                    const startDateInputValue = $v(startDateInput);
+                    const startDateInputFormat = $x(startDateInput).getAttribute("format");
+
+                    // Parse the start date value into a valid date object using the specified format
+                    let $startDateInputValue = apex.date.parse(startDateInputValue, startDateInputFormat);
+
+                    // Add 1 day to the start date to set the default end date (e.g., next day)
+                    $startDateInputValue = apex.date.add($startDateInputValue, 1, apex.date.UNIT.DAY);
+
+                    // Get the format of the end date input
+                    const endDateInputFormat = $x(endDateInput).getAttribute("format");
+
+                    // Format the updated end date (start date + 1 day) according to the end date input's format
+                    const endDateInputValue = apex.date.format($startDateInputValue, endDateInputFormat);
+
+                    // Set the calculated end date value in the end date input field
+                    $s(endDateInput, endDateInputValue);
+
+                    // Mark the end date input as used to avoid overwriting it later
                     usedEndDateInputs.push(endDateInput);
+
                 } else {
+                    // If no start date is available or the end date has already been used, show an error
                     addError(endDateInput, MESSAGES.NS_MUST_SELECT_DATE_TO);
                 }
             }
@@ -83,10 +110,14 @@ function validateDates(item, event, startDateInput, endDateInput) {
         return;
     }
 
-    // Validate if the end date is not earlier than the start date
-    if (endDate < startDate) {
+    // Verify that the end date is after the start date
+    const isDateAfter = apex.date.isAfter(endDate, startDate);
+    if (!isDateAfter) {
+        // Clear errors if the dates are valid, but the end date is not after the start date
         clearError(startDateInput);
         clearError(endDateInput);
+
+        // Depending on which field triggered the validation, display the appropriate message
         if (item.id === startDateInput) {
             addError(startDateInput, MESSAGES.NS_GREATHER_THAN_DATE);
         }
@@ -96,7 +127,7 @@ function validateDates(item, event, startDateInput, endDateInput) {
         return;
     }
 
-    // Clear errors if everything is in order
+    // If the dates are valid, clear any error messages
     clearError(startDateInput);
     clearError(endDateInput);
 }
